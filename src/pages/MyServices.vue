e54a82477e86c68d835380f4bd2127031c3e2d5d
Zyetra <valentin.campagne@viacesi.fr>
2024-04-10 15:56:41 +0200

    My Service Card

--- src/pages/MyServices.vue
+++ src/pages/MyServices.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import MyServiceCard from '../components/MyServiceCard.vue';
 </script>
 
 <template>
-    My Service
+    <div class="d-flex align-center mt-4">
+        <v-btn variant="plain" icon="mdi-arrow-left" @click="$router.go(-1)" />
+        <h1 class="w-100 text-center">Mes Services</h1>
+    </div>
+    <MyServiceCard/>
 </template>
