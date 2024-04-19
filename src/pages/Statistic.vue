122c8d1d5139f307d8d439866ea5957b2c70f579
Zyetra <valentin.campagne@viacesi.fr>
2024-04-19 21:16:06 +0200

    Graph

--- src/pages/Statistic.vue
+++ src/pages/Statistic.vue
@@ -3,48 +3,105 @@ import { onMounted, ref } from "vue";
 import Services from "../service/Services"
 import Users from "../service/Users"
 
-const backEndService = new Services();
 const backEndUsers = new Users();
+const graph = ref<boolean>(false)
+const graph1 = ref<boolean>(false)
+const backEndService = new Services();
+const graph2 =ref<boolean>(false)
 
-const nb_user = ref("")
-const nb_services = ref("")
-const nb_presta = ref("")
+onMounted(() => {
+    backEndUsers.getNbUser().then(body => {
+        const response = body.nb_utilisateurs
+        options.xaxis.categories = response.map((element:Cat)=>{
+            const date = new Date(element.date)
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        })
+        series[0].data = response.map((element:Cat)=>element.count)
+        graph.value = true
+    })
+})
 
-onMounted(()=>{backEndService.getNbServices().then(body=>{
-    nb_services.value = body.nb_services[0]
-})})
+onMounted(() => {
+    backEndUsers.getNbPresta().then(body => {
+        const response = body.nb_prestataires
+        options.xaxis.categories = response.map((element:Cat)=>{
+            const date = new Date(element.date)
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        })
+        series[0].data = response.map((element:Cat)=>element.count)
+        graph1.value = true
+    })
+})
 
-onMounted(()=>{backEndUsers.getNbUser().then(body=>{
-    nb_user.value = body.nb_utilisateurs[0]
-})})
+onMounted(() => {
+    backEndService.getNbServices().then(body => {
+        const response = body.nb_services
+        options.xaxis.categories = response.map((element:Cat)=>{
+            const date = new Date(element.date)
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        })
+        series[0].data = response.map((element:Cat)=>element.count)
+        graph2.value = true
+    })
+})
 
-onMounted(()=>{backEndUsers.getNbPresta().then(body=>{
-    nb_presta.value = body.nb_prestataires[0]
-})})
+interface Cat {
+    count: number;
+    date: Date;
+}
+
+const options = {
+    chart: {
+        type: 'line',
+        foreColor: '#000000',
+        toolbar: {
+            show: false
+        },
+        width: '100%',
+    },
+    xaxis: {
+        categories: [] as string[]
+    },
+    colors: ['#647295', '#9f496e','#CBD4C2'],
+    tooltip: {
+        theme: 'dark'
+    },
+}
+
+let series: { name: string, data: number[] }[] = [{
+    name: 'Utilisateurs',
+    data: []
+}];
 </script>
 
 <template>
-    <div class="d-flex flex-column align-center justify-center gap mt-4">
-        <v-card
-        class="text-center"
-        title="Nombre d'utilisateurs"
-        width="250"
-        >
-            <p class="pa-3">{{ nb_user }}</p>
-        </v-card>
-        <v-card
-        class="text-center"
-        title="Nombre de services"
-        width="250"
-        >
-            <p class="pa-3">{{ nb_services }}</p>
-        </v-card>
-        <v-card
-        class="text-center"
-        title="Nombre de prestataires"
-        width="250"
-        >
-            <p class="pa-3">{{ nb_presta }}</p>
-        </v-card>
-    </div>
+    <v-row>
+        <v-col class="d-flex align-center" cols="12" md="6">
+            <div class="d-flex flex-column justify-start rounded-lg pa-4 ga-8">
+                <span class="text-h4">Nombre d'utilisateurs créés les 7 derniers jours</span>
+                <apexchart v-if="graph" type="bar" :options="options" :series="series"></apexchart>
+            </div>
+        </v-col>
+        <v-col class="d-flex align-center" cols="12" md="6">
+            <div class="d-flex flex-column justify-start rounded-lg pa-4 ga-8">
+                <span class="text-h4">Nombre de prestataires créés les 7 derniers jours</span>
+                <apexchart v-if="graph1" type="bar" :options="options" :series="series"></apexchart>
+            </div>
+        </v-col>
+        <v-col class="d-flex align-center" cols="12" md="6">
+            <div class="d-flex flex-column justify-start rounded-lg pa-4 ga-8">
+                <span class="text-h4">Nombre de services créés les 7 derniers jours</span>
+                <apexchart v-if="graph2" type="bar" :options="options" :series="series"></apexchart>
+            </div>
+        </v-col>
+    </v-row>
 </template>
